c92d03edab9b23aaab66331d5af5c1d6cf140d5a
Matthew Berryhill <matthewberryhill@gmail.com>
2018-04-26 12:20:44 -0600

    Implement GET/route endpoint to transfer data

--- api/app.py
+++ api/app.py
@@ -1,17 +1,20 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 from flask_cors import CORS, cross_origin
 import smartsheet, json
+import requests
 
 
 app = Flask(__name__)
 CORS(app)
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
 app.config['CORS_HEADERS'] = 'Content-Type'
-cors = CORS(
-    app, resources={r"/properties": {"origins": "*"}})
-
+cors = CORS(app, resources={r"/properties": {"origins": "*"}})
 
 access_token = "8iorlqtxib1xix8xsrh4tnbwmq"
+
+token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiZ' \
+        'XhwIjoxNTI1MjgyNTkzLCJmcmFuY2hpc2VfaWQiOjAsIm5hbWUiOiIiLCJyb2xlIj' \
+        'oiIn0.W4hoh0PCbIeh2ckzsM4koBUHI5FJxfMZTZgB70uga_Y'
 
 
 @app.route('/sheets')
@@ -22,7 +25,7 @@ def sheets():
     return resp.to_json()
 
 
-@app.route('/properties', methods=["GET"])
+@app.route('/properties', methods=['GET'])
 @cross_origin('*')
 def properties():
     
@@ -52,6 +55,85 @@ def properties():
     resp['count'] = len(payload)
 
     return json.dumps(resp)
+
+
+@app.route('/port', methods=['GET'])
+def port():
+
+    column_id = request.args.get('column_id')
+    property_param = request.args.get('property_param')
+
+    sheet = ss_client.Sheets.get_sheet(2398906471475076)
+    cols = sheet.columns.to_list()
+    rows = sheet.rows.to_list()
+
+    schema = list()
+    for i, col in enumerate(cols):
+        column = dict()
+        column['title'] = col.title
+        column['id'] = col.id
+        schema.append(column)
+
+    payload = dict()
+    for i, row in enumerate(rows):
+        item = dict()
+        for j, cell in enumerate(row.cells):
+            item[str(cell._column_id)] = cell._value
+        payload[row.id] = item
+
+    param_array = list()
+    param_value = ""
+    lodgix_id = -1
+    for k, v in payload.items():
+        for kk, vv in v.items():
+            if kk == '5806974957840260':
+                lodgix_id = vv
+            if kk == column_id:
+                param_value = vv
+        param_dict = dict()
+        param_dict['lodgix_id'] = lodgix_id
+        param_dict['param_value'] = param_value
+        param_array.append(param_dict)
+
+    uri = 'http://35.184.148.221:7001/restricted/properties'
+
+    resp = requests.get(
+        url=uri,
+        headers={'Authorization': token},
+    )
+
+    update_array = list()
+    properties = json.loads(resp.content)
+    for i, p in enumerate(properties):
+        for ii, pp in enumerate(param_array):
+            if p['lodgix_id'] == pp['lodgix_id']:
+                update = dict()
+                update['param_name'] = property_param
+                update['param_value'] = pp['param_value']
+                update['property_id'] = p['id']
+                update_array.append(update)
+
+    responses = list()
+    for i, ua in enumerate(update_array):
+        if ua['param_name'] != None:
+            data = {ua['param_name']: ua['param_value']}
+            uri = 'http://35.184.148.221:7001/restricted/properties/' \
+                  + ua['property_id']
+
+            headers = {
+                'Authorization': token,
+                'content-type': 'application/json',
+            }
+            resp = requests.put(url=uri, headers=headers, json=data)
+            payload = {
+                'property_id': ua['property_id'],
+                'status_code': resp.status_code,
+                'param_changed': ua['param_name'],
+                'new_param_value': ua['param_value'],
+            }
+            responses.append(payload)
+
+    return json.dumps(responses)
 
 
 @app.route('/columns')
